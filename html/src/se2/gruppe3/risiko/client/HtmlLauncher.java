9e5eaa4ee89ce219f02dba2d75cf0e85ae05ec5c
Nikola Savovic <nisavovic@edu.aau.at>
2021-04-24 16:27:24 +0200

    Neues lIbGdx Projekt ist erstellt

---
+++ html/src/se2/gruppe3/risiko/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package se2.gruppe3.risiko.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import se2.gruppe3.risiko.RisikoGameClass;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new RisikoGameClass();
+        }
+}
