42ebc36cd76bbb7db39ecf8e5ea7ac0d8bdf2886
pesoellnbaue <pesoellnbaue@edu.aau.at>
2021-05-11 11:40:00 +0200

    Commit to solve Sonar-Issues

--- app/src/main/java/at/aau/risiko/MainActivity.java
+++ app/src/main/java/at/aau/risiko/MainActivity.java
@@ -18,47 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView(R.layout.activity_dice);
+        setContentView(R.layout.activity_dice);
 
-        setContentView(R.layout.activity_main);
-
-        Button send = findViewById(R.id.sendToServer);
-        send.setOnClickListener(v -> {
-            sendMessage();
-        });
-
-
-        try {
-            startServer();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void startServer() throws IOException {
-        NetworkServer server = new NetworkServerKryo();
-        server.start();
-
-        String host = "127.0.0.1";
-
-        NetworkThread thread = new NetworkThread(host);
-
-        thread.start();
-        try {
-            thread.join();
-        } catch (Exception e) {
-            System.out.println("An error occured");
-        }
-    }
-
-
-    private void sendMessage()
-    {
-        TextMessage m = new TextMessage();
-        m.text = "Some text";
-
-        NetworkClientKryo c = new NetworkClientKryo();
-        c.sendMessage(m);
 
     }
 }
