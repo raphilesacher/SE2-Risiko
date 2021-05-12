731302a748e45e8d25c88a8d3e37243e1b7c3f53
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-05-12 20:25:14 +0200

    made a button and set it's onClickListener which calls function openDialog
    this is just for testing my feature's functionality

--- app/src/main/java/at/aau/risiko/MainActivity.java
+++ app/src/main/java/at/aau/risiko/MainActivity.java
@@ -3,6 +3,8 @@ package at.aau.risiko;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //just for testing the dice feature
+        Button diceBtn = findViewById(R.id.diceBtn);
+        diceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+
+    }
+
+    private void openDialog() {
     }
 }
