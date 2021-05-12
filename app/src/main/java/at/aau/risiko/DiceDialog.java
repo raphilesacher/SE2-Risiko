3f00ef946f6297ca0c9b4c251b28158d1496a1c1
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-05-12 20:21:36 +0200

    began to override onCreatteDialog in Dialog class so that the dialog is build with the layout for the dice section.

--- app/src/main/java/at/aau/risiko/DiceDialog.java
+++ app/src/main/java/at/aau/risiko/DiceDialog.java
@@ -1,7 +1,27 @@
 package at.aau.risiko;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 public class DiceDialog extends AppCompatDialogFragment {
+    AlertDialog dialog;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_dice, null);
+
+        builder.setView(view);
+
+
+
+        return builder.create();
+    }
+
 
 }
