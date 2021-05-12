f854af2d5338bcfccbc2c3e077611b543164c9dc
Raphael Lesacher <r1lesacher@edu.aau.at>
2021-05-12 18:26:24 +0200

    created class Dice with fields, constructor, getter and setter.

---
+++ app/src/main/java/at/aau/risiko/Dice.java
@@ -0,0 +1,28 @@
+package at.aau.risiko;
+
+public class Dice {
+    private int eyeNumber;
+    private String type;
+    
+    public Dice(int eyeNumber, String type) {
+        this.eyeNumber = eyeNumber;
+        this.type = type;
+    }
+    public void setEyeNumber(int eyeNumber) {
+        this.eyeNumber = eyeNumber;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getEyeNumber() {
+        return eyeNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+}
