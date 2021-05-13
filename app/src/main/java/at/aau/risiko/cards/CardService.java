3bcf5a58b3fbfcfbfef142fb25efda7fef8c906f
squishyjoski <kris.rehsmann@gmail.com>
2021-05-13 14:46:08 +0200

    Methode drawCardFromList
    erstellen einer Methode, um eine Karte aus der Liste zu ziehen

--- app/src/main/java/at/aau/risiko/cards/CardService.java
+++ app/src/main/java/at/aau/risiko/cards/CardService.java
@@ -1,20 +1,43 @@
 package at.aau.risiko.cards;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CardService {
 
+    ArrayList<Card> cardDeck = initCardListForStart();
 
-    public static void initCardListForStart() {
+    public static ArrayList<Card> initCardListForStart() {
 
        ArrayList<Card> cardList = CardList.createCardList();
-
-    }
-
-    public static void drawCardFromCardList(){
-
-        
+       return cardList;
 
 
     }
+
+
+
+    public String drawCardFromCardList(){
+
+        String drawnCard = "";
+        boolean succes =false;
+
+        while(!succes) {
+
+            int bound = cardDeck.size();
+            Random rand = new Random();
+            int randomInt = rand.nextInt(bound);
+            if(!cardDeck.get(randomInt).isCardIsdrawn()){
+                drawnCard = cardDeck.get(randomInt).cardName;
+                cardDeck.get(randomInt).setCardIsdrawn(true);
+                succes=true;
+            }
+
+        }
+
+        return drawnCard;
+    }
+
+
+
 }
