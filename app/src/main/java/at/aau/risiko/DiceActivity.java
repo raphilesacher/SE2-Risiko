8a22fc364c7840368572914e2871d1ab4baa880b
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2021-05-18 11:23:45 +0200

    Dice (#51)
    * created activity_dice.xml
    
    * created UI for dice roll feature in activity_dice(land)
    
    * changed app theme to android:theme="@style/Theme.AppCompat.DayNight.NoActionBar" in AndroidManifest.xml
    
    * automatic changes
    
    * created class Dice with fields, constructor, getter and setter.
    
    * created class DiceDialog for the implementation of the Dialog that appears if a dice roll is needed in the game
    
    * implemented method diceRoll in class Dice and changed the constructor
    
    * began to override onCreatteDialog in Dialog class so that the dialog is build with the layout for the dice section.
    
    * made a button and set it's onClickListener which calls function openDialog
    
    this is just for testing my feature's functionality
    
    * implemented onDialog in MainActivity
    
    * Made an activity out of DiceDialog because I was not able to use sensors in a Dialog
    
    * implemented all required methods for the use of the Accelerometer
    
    * wrote onClickListener in MainActivity which starts the DiceActivity
    
    * added the DiceActivity to the manifest file
    
    * implemented cheating function
    
    * added Internet permission and acces_network_state to manifest.xml
    
    * commented out my button in main.xml
    
    * deleted permissions for internet and network access because they were already there
    
    * deleted commented out button because travis failed
    
    Co-authored-by: Daniel Egger <68168885+castanie@users.noreply.github.com>

---
+++ app/src/main/java/at/aau/risiko/DiceActivity.java
@@ -0,0 +1,93 @@
+package at.aau.risiko;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import java.util.Objects;
+
+import static android.content.Context.SENSOR_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+public class DiceActivity extends AppCompatActivity implements SensorEventListener {
+    private AlertDialog dialog;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private TextView diceNum;
+    private TextView accel;
+    //dice should only be rolled if acceleration is > SHAKE_THRESHOLD
+    final static int SHAKE_THRESHOLD = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+         diceNum = (TextView)findViewById(R.id.diceNumber);
+         accel = (TextView)findViewById(R.id.acceleration);
+
+        //variables for building the dialog
+        /*AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_dice, null);*/
+        //variables to track sensor activity
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+        //builder.setView(view);
+
+
+
+        //return builder.create();
+    }
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        //variables for tracking the motion of the device on x-, y- and z-axis
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+        //calculate the movement value
+        double accelerationValueCurrent = Math.sqrt((x*x + y*y + z*z)) - SensorManager.GRAVITY_EARTH;
+        //if accelerationValueCurrent > SHAKE_THRESHOLD call rollDice
+        Dice dice = new Dice("attacker");
+        if(accelerationValueCurrent > SHAKE_THRESHOLD) {
+
+            int num = dice.diceRoll();
+            diceNum.setText("dice have been rolled:" + num);
+            accel.setText("Acceleration: " + (int)accelerationValueCurrent);
+        }
+        //cheat function
+        if(accelerationValueCurrent > 30) {
+            dice.setEyeNumber(6);
+            diceNum.setText("dice have been rolled" + dice.getEyeNumber());
+            accel.setText("Acceleration: " + (int)accelerationValueCurrent);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
