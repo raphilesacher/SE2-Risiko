0d7b9601a68533a0a40e403cc2a7fa74bb88699b
pesoellnbaue <pesoellnbaue@edu.aau.at>
2021-05-19 10:33:25 +0200

    Clean up unused States #1: Move Logic from State-Strenghten to Draft-State in Core package

--- app/src/main/java/at/aau/risiko/core/DraftState.java
+++ app/src/main/java/at/aau/risiko/core/DraftState.java
@@ -2,41 +2,54 @@ package at.aau.risiko.core;
 
 import android.view.View;
 
+import at.aau.risiko.Continent;
+import at.aau.risiko.Country;
+
 public class DraftState extends State {
 
-    public DraftState(Game game) {
+    at.aau.risiko.Continent continent = new Continent();
+    at.aau.risiko.Country country = new at.aau.risiko.Country();
+    int bonus = continent.getBonus();
+    at.aau.risiko.Country[] countries = continent.getCountries();
+    int maxArmys = maxArmys(bonus, countries);
+    public DraftState(Game game, int maxArmys) {
         super(game);
-        //TODO Auto-generated constructor stub
+        this.maxArmys = maxArmys;
     }
-
-    /**
-     * 
-     * The constructor must calculate the armies available to
-     * the player.
-     * 
-     * handleInput():
-     * Deploy armies in country if owned by player.
-     * Change state if all armies have been used.
-     * 
-     * changeState():
-     * Transition to AttackState.
-     * 
-     */
-
-    // Methods:
 
     @Override
     public void handleInput(View view) {
-        // TODO Auto-generated method stub
-        
+
+        // TODO Implement method Calls for Frontend
+
+    }
+
+    public int getMaxArmys() {
+        return maxArmys;
+    }
+
+    private int maxArmys(int bonus, Country[] cs) {
+        int numOfCountries = cs.length;
+        int armys = numOfCountries / 3;
+
+        if (bonus > 0) {
+            armys = armys + bonus;
+        }
+        return armys;
+    }
+
+    private int strenghtenCountry(int choosen) {
+        int actualArmys = country.getArmys();
+        int newArmys = actualArmys + choosen;
+        country.setArmys(newArmys);
+
+        maxArmys = maxArmys - choosen;
+        return maxArmys;
     }
 
     @Override
     public void changeState() {
-        // TODO Auto-generated method stub
-        
-    }
+        // TODO Implement swith to Attack
 
-    
-    
+    }
 }
