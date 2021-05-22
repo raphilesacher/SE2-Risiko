c80821eecc4f03cf194d7822d151ad0b19ed8320
pesoellnbaue <pesoellnbaue@edu.aau.at>
2021-05-22 12:01:43 +0200

    Added test methods, need o create instance of Game

--- app/src/test/java/at/aau/risiko/TestDraftState.java
+++ app/src/test/java/at/aau/risiko/TestDraftState.java
@@ -1,4 +1,21 @@
 package at.aau.risiko;
 
+import org.junit.Test;
+
+import at.aau.risiko.core.DraftState;
+import at.aau.risiko.core.Game;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestDraftState {
+
+    @Test
+    public void calculateStrenghtGreater3 () {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void calculateStrenghtSmaller3 () {
+        assertEquals(4, 2 + 2);
+    }
 }
