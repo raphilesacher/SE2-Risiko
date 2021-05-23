a890ab1e18b7583a2e1e074a30ebfcc2fcd4bc09
squishyjoski <kris.rehsmann@gmail.com>
2021-05-23 19:04:05 +0200

    show cards that have been drawn

--- app/src/main/java/at/aau/risiko/CardActivity.java
+++ app/src/main/java/at/aau/risiko/CardActivity.java
@@ -2,13 +2,11 @@ package at.aau.risiko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,14 +16,10 @@ import at.aau.risiko.core.HandDeck;
 
 public class CardActivity extends AppCompatActivity {
 
-    CardList cardDeck = new CardList();
-    HandDeck handDeck = new HandDeck();
-
-
-
+    CardList cardDeck;
+    HandDeck handDeck;
 
     LinearLayout linearLayout_handDeck;
-    
 
     ArrayList<Integer> imgIds;
     LayoutInflater inflater;
@@ -37,26 +31,130 @@ public class CardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
 
+        cardDeck = new CardList();
+        handDeck = new HandDeck();
         cardDeck.fillUpCardlistForStart();
+
+        drawCards();
 
 
        inflater = LayoutInflater.from(this);
-       initData();
+       initDataForShowingHandDeck();
        showCards();
+
        //initView();
        //addCardToSelection();
 
 
     }
 
+    private void drawCards(){
 
-    private void initData()
+        handDeck.addCardToHandDeck(cardDeck.drawCardFromCardList());
+        handDeck.addCardToHandDeck(cardDeck.drawCardFromCardList());
+        handDeck.addCardToHandDeck(cardDeck.drawCardFromCardList());
+        handDeck.addCardToHandDeck(cardDeck.drawCardFromCardList());
+        handDeck.addCardToHandDeck(cardDeck.drawCardFromCardList());
+      
+
+    }
+
+
+    private void initDataForShowingHandDeck()
     {
         imgIds=new ArrayList<>();
-        imgIds.add(R.drawable.ca_alaska);
-        imgIds.add(R.drawable.ca_argentina);
-        imgIds.add(R.drawable.ca_brazil);
-        imgIds.add(R.drawable.ca_canada);
+
+        for (int i = 0; i < handDeck.size(); i++)
+        {
+            String card = handDeck.getCardFromHandDeck(i);
+
+            if(card.equals("Alaska")){
+                imgIds.add(R.drawable.ca_alaska);
+            }
+            if(card.equals("Greenland")){
+                imgIds.add(R.drawable.ca_greenland);
+            }
+            if(card.equals("Canada")){
+                imgIds.add(R.drawable.ca_canada);
+            }
+            if(card.equals("Central America")){
+                imgIds.add(R.drawable.ca_central_amerika);
+            }
+            if(card.equals("Venezuela")){
+                imgIds.add(R.drawable.ca_venezuela);
+            }
+            if(card.equals("Peru")){
+                imgIds.add(R.drawable.ca_peru);
+            }
+            if(card.equals("Brazil")){
+                imgIds.add(R.drawable.ca_brazil);
+            }
+            if(card.equals("Argentina")){
+                imgIds.add(R.drawable.ca_argentina);
+            }
+            if(card.equals("North Africa")){
+                imgIds.add(R.drawable.ca_north_africa);
+            }
+            if(card.equals("East Africa")){
+                imgIds.add(R.drawable.ca_east_africa);
+            }
+            if(card.equals("Congo")){
+                imgIds.add(R.drawable.ca_congo);
+            }
+            if(card.equals("South Africa")) {
+                imgIds.add(R.drawable.ca_south_africa);
+            }
+            if (card.equals("Scandinavia")) {
+                    imgIds.add(R.drawable.ca_scandinavia);
+            }
+            if (card.equals("Ukraine")) {
+                imgIds.add(R.drawable.ca_ukraine);
+            }
+            if (card.equals("Western Europe")) {
+                imgIds.add(R.drawable.ca_western_europe);
+            }
+            if (card.equals("Indonesia")) {
+                imgIds.add(R.drawable.ca_indonesia);
+            }
+            if (card.equals("Western Australia")) {
+                imgIds.add(R.drawable.ca_western_australia);
+            }
+            if (card.equals("Eastern Australia")) {
+                imgIds.add(R.drawable.ca_eastern_australia);
+            }
+            if (card.equals("Siam")) {
+                imgIds.add(R.drawable.ca_siam);
+            }
+            if (card.equals("India")) {
+                imgIds.add(R.drawable.ca_india);
+            }
+            if (card.equals("China")) {
+                imgIds.add(R.drawable.ca_china);
+            }
+            if (card.equals("Mongolia")) {
+                imgIds.add(R.drawable.ca_mongolia);
+            }
+            if (card.equals("Siberia")) {
+                imgIds.add(R.drawable.ca_siberia);
+            }
+            if (card.equals("Ural")) {
+                imgIds.add(R.drawable.ca_ural);
+            }
+            if (card.equals("Middle East")) {
+                imgIds.add(R.drawable.ca_middle_east);
+            }
+            if (card.equals("USA")) {
+                imgIds.add(R.drawable.ca_usa);
+            }
+            if (card.equals("Joker1")) {
+                imgIds.add(R.drawable.ca_joker1);
+            }
+            if (card.equals("Joker2")) {
+                imgIds.add(R.drawable.ca_joker2);
+            }
+        }
+
+
       }
 
 
