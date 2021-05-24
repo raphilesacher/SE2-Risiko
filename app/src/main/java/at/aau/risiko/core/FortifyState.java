d5ede806e8450741dfe96a13110655af6ea3d245
pesoellnbaue <pesoellnbaue@edu.aau.at>
2021-05-24 17:32:07 +0200

    Started implementation of Fortfy State

--- app/src/main/java/at/aau/risiko/core/FortifyState.java
+++ app/src/main/java/at/aau/risiko/core/FortifyState.java
@@ -1,14 +1,21 @@
 package at.aau.risiko.core;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
 
 public class FortifyState extends State {
 
-    Country donor;
-    Country recipient;
+    private Country donor;
+    private Country recipient;
+    Player p = game.getPlayers()[game.getIndex()];
 
     public FortifyState(Game game) {
         super(game);
+        donor = null;
+        recipient = null;
         //TODO Auto-generated constructor stub
     }
 
@@ -17,7 +24,7 @@ public class FortifyState extends State {
      * Select donor country.
      * Select recipient country.
      * Change state if selection is valid, otherwise reset.
-     *
+     * <p>
      * changeState():
      * Send message to server, then transition to ObserverState.
      */
@@ -26,7 +33,41 @@ public class FortifyState extends State {
     @Override
     public void handleInput(View view) {
         // TODO Auto-generated method stub
-        changeState();
+
+
+        Country clicked = game.buttonMap.get(view.getId());
+
+        if (donor == null) {
+            donor = clicked;
+        } else if (recipient == null) {
+            boolean valid = false;
+            for (Country c : clicked.getNeighbors()) {
+                if (c == donor) {
+                    recipient = clicked;
+                    valid = true;
+                    break;
+                }
+            }
+            if (valid) {
+
+                //move one Army from donor to recipient
+                
+                int donorArmys = donor.getArmies() - 1;
+                int recipientArmys = recipient.getArmies() + 1;
+
+                donor.setArmies(donorArmys);
+                recipient.setArmies(recipientArmys);
+
+                Button button = (Button) view;
+                button.setText(Integer.toString(donorArmys));
+
+                changeState();
+            }
+        } else {
+            donor = null;
+            recipient = null;
+        }
+
     }
 
     @Override
