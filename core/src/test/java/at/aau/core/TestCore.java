397e6dd7588dcd4b8c4f388cd67e078c0480d6b7
Daniel Egger <dan.egg@outlook.com>
2021-05-26 15:17:18 +0200

    Add test package to Core module

---
+++ core/src/test/java/at/aau/core/TestCore.java
@@ -0,0 +1,14 @@
+package at.aau.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCore {
+
+    @Test
+    public void testNothing() {
+        assertEquals(0, 0);
+    }
+
+}
