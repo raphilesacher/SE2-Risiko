8769a758af0706cbda49c314ec90c06a346bfe3b
Daniel Egger <dan.egg@outlook.com>
2021-05-28 20:12:19 +0200

    Add basic button creation logic to MapActivity

--- app/src/main/java/at/aau/risiko/MapActivity.java
+++ app/src/main/java/at/aau/risiko/MapActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.Group;
 
 import java.util.HashMap;
@@ -32,7 +33,7 @@ public class MapActivity extends AppCompatActivity {
 
     Game game;
 
-    HashMap<Integer, Country> buttonMapping;
+    HashMap<Integer, Country> countryMapping;
     HashMap<Integer, Player> playerMapping;
     HashMap<Integer, int[]> neighborMapping;
 
@@ -41,70 +42,12 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-
-        GameClient.getInstance().sendMessage(new ReadyMessage());
-
-
-        // Find all buttons in view and link to countries:
-        buttonMapping = new HashMap<Integer, Country>();
-        int[] buttons = ((Group) findViewById(R.id.group)).getReferencedIds();
-        for (int button : buttons) {
-            buttonMapping.put(button, new Country(findViewById(button).getContentDescription().toString()));
-            // Log.i("COUNTRY LISTED", buttonMapping.get(button).getName());
-        }
-
-
-        // TODO:  PUT DATA INTO EXTERNAL JSON?
-        // Put all countries bordering a country into a hashtable:
-        neighborMapping = new HashMap<Integer, int[]>();
-        neighborMapping.put(R.id.buttonAlaska, new int[]{R.id.buttonOntario, R.id.buttonYakutsk});
-        neighborMapping.put(R.id.buttonArgentina, new int[]{R.id.buttonBrazil, R.id.buttonPeru});
-        neighborMapping.put(R.id.buttonBrazil, new int[]{R.id.buttonArgentina, R.id.buttonPeru, R.id.buttonVenezuela});
-        neighborMapping.put(R.id.buttonCentralAmerica, new int[]{R.id.buttonEastCoast, R.id.buttonVenezuela, R.id.buttonWestCoast});
-        neighborMapping.put(R.id.buttonChina, new int[]{R.id.buttonIndia, R.id.buttonMongolia, R.id.buttonSiam, R.id.buttonSiberia, R.id.buttonUral, R.id.buttonYakutsk});
-        neighborMapping.put(R.id.buttonCongo, new int[]{R.id.buttonEthiopia, R.id.buttonNorthAfrica, R.id.buttonSouthAfrica});
-        neighborMapping.put(R.id.buttonEastAustralia, new int[]{R.id.buttonNewGuinea, R.id.buttonWestAustralia});
-        neighborMapping.put(R.id.buttonEastCoast, new int[]{R.id.buttonCentralAmerica, R.id.buttonOntario, R.id.buttonQuebec, R.id.buttonWestCoast});
-        neighborMapping.put(R.id.buttonEgypt, new int[]{R.id.buttonEthiopia, R.id.buttonMiddleEast, R.id.buttonNorthAfrica, R.id.buttonWestEurope});
-        neighborMapping.put(R.id.buttonEthiopia, new int[]{R.id.buttonCongo, R.id.buttonEgypt, R.id.buttonMiddleEast, R.id.buttonNorthAfrica, R.id.buttonSouthAfrica});
-        neighborMapping.put(R.id.buttonGreenland, new int[]{R.id.buttonOntario, R.id.buttonQuebec, R.id.buttonScandinavia, R.id.buttonWestEurope});
-        neighborMapping.put(R.id.buttonIndia, new int[]{R.id.buttonChina, R.id.buttonMiddleEast, R.id.buttonSiam, R.id.buttonUral});
-        neighborMapping.put(R.id.buttonIndonesia, new int[]{R.id.buttonNewGuinea, R.id.buttonSiam, R.id.buttonWestAustralia});
-        neighborMapping.put(R.id.buttonMiddleEast, new int[]{R.id.buttonEgypt, R.id.buttonEthiopia, R.id.buttonIndia, R.id.buttonUkraine, R.id.buttonUral});
-        neighborMapping.put(R.id.buttonMongolia, new int[]{R.id.buttonChina, R.id.buttonSiberia});
-        neighborMapping.put(R.id.buttonNewGuinea, new int[]{R.id.buttonEastAustralia});
-        neighborMapping.put(R.id.buttonNorthAfrica, new int[]{R.id.buttonCongo, R.id.buttonEgypt, R.id.buttonWestEurope});
-        neighborMapping.put(R.id.buttonOntario, new int[]{R.id.buttonAlaska, R.id.buttonEastCoast, R.id.buttonGreenland, R.id.buttonQuebec, R.id.buttonWestCoast});
-        neighborMapping.put(R.id.buttonPeru, new int[]{R.id.buttonArgentina, R.id.buttonBrazil, R.id.buttonVenezuela});
-        neighborMapping.put(R.id.buttonQuebec, new int[]{R.id.buttonEastCoast, R.id.buttonGreenland, R.id.buttonOntario});
-        neighborMapping.put(R.id.buttonScandinavia, new int[]{R.id.buttonGreenland, R.id.buttonUkraine, R.id.buttonUral, R.id.buttonWestEurope});
-        neighborMapping.put(R.id.buttonSiam, new int[]{R.id.buttonChina, R.id.buttonIndia, R.id.buttonIndonesia});
-        neighborMapping.put(R.id.buttonSiberia, new int[]{R.id.buttonChina, R.id.buttonMongolia, R.id.buttonUral, R.id.buttonYakutsk});
-        neighborMapping.put(R.id.buttonSouthAfrica, new int[]{R.id.buttonCongo, R.id.buttonEthiopia});
-        neighborMapping.put(R.id.buttonUkraine, new int[]{R.id.buttonMiddleEast, R.id.buttonScandinavia, R.id.buttonUral, R.id.buttonWestEurope});
-        neighborMapping.put(R.id.buttonUral, new int[]{R.id.buttonChina, R.id.buttonIndia, R.id.buttonMiddleEast, R.id.buttonScandinavia, R.id.buttonSiberia});
-        neighborMapping.put(R.id.buttonVenezuela, new int[]{R.id.buttonBrazil, R.id.buttonCentralAmerica, R.id.buttonPeru});
-        neighborMapping.put(R.id.buttonWestAustralia, new int[]{R.id.buttonEastAustralia, R.id.buttonIndonesia});
-        neighborMapping.put(R.id.buttonWestCoast, new int[]{R.id.buttonCentralAmerica, R.id.buttonEastCoast, R.id.buttonOntario});
-        neighborMapping.put(R.id.buttonWestEurope, new int[]{R.id.buttonEgypt, R.id.buttonGreenland, R.id.buttonNorthAfrica, R.id.buttonScandinavia, R.id.buttonUkraine});
-        neighborMapping.put(R.id.buttonYakutsk, new int[]{R.id.buttonAlaska, R.id.buttonChina, R.id.buttonSiberia});
-
-
-        // Convert name mapping to neighbour mapping:
-        for (int i : neighborMapping.keySet()) {
-            for (int j : neighborMapping.get(i)) {
-                buttonMapping.get(i).addNeighbor(buttonMapping.get(j));
-                // Log.i("REGISTERED NEIGHBOR", buttonMapping.get(i).getName() + " added neighbor " + buttonMapping.get(j).getName());
-            }
-        }
-
-
         // Start game:
         // TODO: CHANGE PLAYER ARRAY TO REFLECT PLAYERS CONNECTED TO SERVER
         game = new Game(new Player[]{
                 new Player("Uno", 0xFFFFCC00),
                 new Player("Due", 0xFFFF00CC)},
-                buttonMapping, this);
+                countryMapping, this);
 
         GameClient.getInstance().registerCallback(new Callback<BaseMessage>() {
             @Override
@@ -124,11 +67,47 @@ public class MapActivity extends AppCompatActivity {
             }
         });
 
-        // Add every country to the list of available countries
-        for (Country country : buttonMapping.values()) {
+
+        // Add buttons from JSON country data:
+        ConstraintLayout.LayoutParams constParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        constParams.topToTop = 0;
+        constParams.bottomToBottom = 0;
+        constParams.startToStart = 0;
+        constParams.endToEnd = 0;
+
+        // THIS GETS SET IN THE LOOP THAT ITERATES OVER THE JSON:
+        constParams.horizontalBias = 0.5f;
+        constParams.verticalBias = 0.5f;
+
+
+        Button testButton = new Button(this);
+        testButton.setLayoutParams(constParams);
+        ((ConstraintLayout) findViewById(R.id.constraintLayout)).addView(testButton);
+
+        for (int id : ((Group) findViewById(R.id.group)).getReferencedIds()) {
+            Log.i(String.valueOf(findViewById(id).getContentDescription()), ((ConstraintLayout.LayoutParams) findViewById(id).getLayoutParams()).verticalBias + " : " + ((ConstraintLayout.LayoutParams) findViewById(id).getLayoutParams()).horizontalBias);
+        }
+
+
+        // TODO: PUT THIS LOGIC DIRECTLY INTO BUTTON GENERATION!
+        // Find all buttons in view and link to countries:
+        countryMapping = new HashMap<Integer, Country>();
+        for (Country country : countryMapping.values()) {
             game.getAvailableCountries().add(country);
         }
 
+        // Put all countries bordering a country into a hashtable:
+        neighborMapping = new HashMap<Integer, int[]>();
+
+        // Convert name mapping to neighbour mapping:
+        for (int i : neighborMapping.keySet()) {
+            for (int j : neighborMapping.get(i)) {
+                countryMapping.get(i).addNeighbor(countryMapping.get(j));
+                // Log.i("REGISTERED NEIGHBOR", buttonMapping.get(i).getName() + " added neighbor " + buttonMapping.get(j).getName());
+            }
+        }
 
         // Add players to side layout
         playerMapping = new HashMap<Integer, Player>();
@@ -138,46 +117,37 @@ public class MapActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT,
                 LinearLayout.LayoutParams.WRAP_CONTENT);
         params.setMargins(0, 8, 32, 8);
+
         for (Player player : game.getPlayers()) {
             ImageView avatar = new ImageView(this);
+
             avatar.setId(View.generateViewId());
-            avatar.setImageResource(R.drawable.ic_army_counter);
             avatar.setLayoutParams(params);
-            // TODO: GET RID OF API DEPENDENCY!
+            avatar.setImageResource(R.drawable.ic_army_counter);
             avatar.setImageTintList(ColorStateList.valueOf(player.getColor()));
             avatar.setImageTintMode(PorterDuff.Mode.MULTIPLY);
+
             layout.addView(avatar, LinearLayout.LayoutParams.WRAP_CONTENT);
+
             playerMapping.put(avatar.getId(), player);
         }
 
-
-        //open CardActivity by clicking on Card-Button
-        Button cardButton = findViewById(R.id.buttonCards);
-        cardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                openCardActivity();
-            }
-
-        });
-
-    }
-
-
-    //method for opening CardActivity
-    public void openCardActivity() {
-        Intent intent = new Intent(this, CardActivity.class);
-        startActivity(intent);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        GameClient.getInstance().sendMessage(new ReadyMessage());
     }
 
+    // Country button:
     public void onClick(View view) {
         game.handleInput(view);
+    }
+
+    // Cards button:
+    public void openCardActivity(View view) {
+        startActivity(new Intent(this, CardActivity.class));
     }
 
 }
