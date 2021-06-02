ad09c36910af230896666faa4ff665b250222c03
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2021-06-02 17:24:36 +0200

    started implementing the DiceActivityAttacker class

--- app/src/main/java/at/aau/risiko/DiceActivityAttacker.java
+++ app/src/main/java/at/aau/risiko/DiceActivityAttacker.java
@@ -3,13 +3,67 @@ package at.aau.risiko;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DiceActivityAttacker extends AppCompatActivity implements SensorEventListener {
+    /*these variables are needed to track the sensor event*/
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    /*UI Variables*/
+    private ImageView diceOneAttack;
+    private ImageView diceTwoAttack;
+    private ImageView diceThreeAttack;
+
+    /**
+     *ToDo: replace this Variables with getNumAttackers() from Daniel's feature to set exactly the amount of dices needed
+     */
+    /*This variable is needed to set the exact amount of dices needed*/
+    int numAttackers = 3;
+    /*This boolean needs to be set to true after the dices have been rolled to send to the Defender so the UI can be updated*/
+    boolean isShaken = false;
+    /*if hasShakenDefender == true the Attacker is allowed to roll the dices*/
+    boolean hasShakenDefender = false;
+    //dice should only be rolled if acceleration is > SHAKE_THRESHOLD
+    final static int SHAKE_THRESHOLD = 3;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+        diceOneAttack = findViewById(R.id.diceOneAttack);
+        diceTwoAttack = findViewById(R.id.diceTwoAttack);
+        diceThreeAttack = findViewById(R.id.diceThreeAttack);
+        /*these variables are only for updating the UI with the according to the server message*/
+        ImageView diceOneDefense = findViewById(R.id.diceOneDefense);
+        ImageView diceTwoDefense = findViewById(R.id.diceTwoDefense);
+        
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        Button closeBtn = (Button)findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //close the activity
+                finish();
+            }
+        });
+
+        /**
+         * ToDo: read server message from DiceActivityDefender and set hasShakenDefender and update the UI
+          */
+
     }
 
     @Override
--- app/src/main/java/at/aau/risiko/DiceActivityDefender.java
+++ app/src/main/java/at/aau/risiko/DiceActivityDefender.java
@@ -3,14 +3,25 @@ package at.aau.risiko;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DiceActivityDefender extends AppCompatActivity implements SensorEventListener {
+    /*these variables are needed to track the sensor event*/
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    /*UI Variables*/
+    private ImageView diceOneDefense;
+    private ImageView diceTwoDefense;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
