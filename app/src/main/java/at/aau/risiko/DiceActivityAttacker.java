00867f8072d7e78b0a7b9a191b04f61f9cf34e80
Raphael Lesacher <80222395+raphilesacher@users.noreply.github.com>
2021-06-02 16:29:52 +0200

    created DiceActivityAttacker and DiceActivityDefender to seperate the dice action of the attacker and the one of the defender
    This should prevent that the attacker rolls dice for defender and vice versa

---
+++ app/src/main/java/at/aau/risiko/DiceActivityAttacker.java
@@ -0,0 +1,24 @@
+package at.aau.risiko;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiceActivityAttacker extends AppCompatActivity implements SensorEventListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
